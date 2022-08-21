ce33844b4ce4e544f9390f8b67f87833b4eeb052
seventhwonder-amiga <82057491+seventhwonder-amiga@users.noreply.github.com>
2022-08-22 00:16:54 +0300

    Add files via upload

---
+++ common/settings.h
@@ -0,0 +1,49 @@
+#ifndef SETTINGS_H
+#define SETTINGS_H
+
+#include <string>
+class INIClass;
+
+class SettingsClass
+{
+public:
+    SettingsClass();
+
+    void Load(INIClass& ini);
+    void Save(INIClass& ini);
+
+    struct
+    {
+        bool RawInput;
+        int Sensitivity;
+        bool ControllerEnabled;
+        int ControllerPointerSpeed;
+    } Mouse;
+
+    struct
+    {
+        int WindowWidth;
+        int WindowHeight;
+        bool Windowed;
+        bool Boxing;
+        std::string BoxingAspectRatio;
+        int Width;
+        int Height;
+        int FrameLimit;
+        int InterpolationMode;
+        bool HardwareCursor;
+        bool DOSMode;
+        std::string Scaler;
+        std::string Driver;
+        std::string PixelFormat;
+    } Video;
+
+    struct
+    {
+        bool MouseWheelScrolling;
+    } Options;
+};
+
+extern SettingsClass Settings;
+
+#endif
